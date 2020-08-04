6ff82a607669fd56fc825caad8195353f8e1602b
Jessica <duellje@oregonstate.edu>
2020-08-04 14:33:07 -0400

    post router get /:id works

--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -2,13 +2,36 @@ const express = require("express");
 const router = express.Router();
 const Data = require("../data/db");
 
+//returns an array of all the post objects contained in the database
 router.get("/", (req, res) => {
   Data.find()
     .then((posts) => {
       res.status(200).json({ data: posts });
     })
     .catch((err) => {
-      res.status(500).json({ error: err });
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved" });
+    });
+});
+
+//returns the post object with the specified id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Data.findById(id)
+    .then((post) => {
+      if (post[0] === undefined) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      } else {
+        res.status(200).json({ data: post });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved" });
     });
 });
 
