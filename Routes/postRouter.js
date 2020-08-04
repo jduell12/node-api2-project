0e62b7dda2077db08ab43e2694134c3dfdf90e48
Jessica <duellje@oregonstate.edu>
2020-08-04 14:22:53 -0400

    get request works for /api/posts

--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Data = require("../data/db");
 
 router.get("/", (req, res) => {
-  res.status(200).send("In post router");
+  Data.find()
+    .then((posts) => {
+      res.status(200).json({ data: posts });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = router;
