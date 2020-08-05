fa368be9b3a1d5659fa0920478571cba90a9d670
Jessica <duellje@oregonstate.edu>
2020-08-04 20:24:01 -0400

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch, Route, useHistory, Link } from "react-router-dom";
+
+//components
+import Posts from "./components/Posts";
 
 function App() {
-  return <div className="App"></div>;
+  const history = useHistory();
+
+  const [postInfo, setInfo] = useState({
+    title: "",
+    contents: "",
+  });
+
+  const [comments, setComments] = useState({
+    text: "",
+  });
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/posts">
+          <Posts posts={postInfo} comments={comments} />
+        </Route>
+      </Switch>
+      <button onClick={() => history.push("/posts")}>Get Posts</button>
+      <Link to="/">Home</Link>
+    </div>
+  );
 }
 
 export default App;
