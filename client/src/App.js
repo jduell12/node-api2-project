03c0f4e716b831196460af3392b2baf063e45532
Jessica <duellje@oregonstate.edu>
2020-08-04 21:06:35 -0400

    working on stretch

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { Switch, Route, useHistory, Link } from "react-router-dom";
 //components
 import Posts from "./components/Posts";
 
+//context
+import { PostContext } from "./context/PostContext";
+
 function App() {
   const history = useHistory();
 
@@ -18,16 +21,22 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/posts">
-          <Posts posts={postInfo} comments={comments} />
-        </Route>
-        <Route>
-          <div>
-            <h1>Post Database</h1>
-            <button onClick={() => history.push("/posts")}>Get Posts</button>
-          </div>
-        </Route>
+        <PostContext.Provider
+          value={{ postInfo, setInfo, comments, setComments }}
+        >
+          <Route exact path="/posts">
+            <Posts querykey={"post"} />
+          </Route>
+
+          <Route exact path="/">
+            <div>
+              <h1>Post Database</h1>
+              <button onClick={() => history.push("/posts")}>Get Posts</button>
+            </div>
+          </Route>
+        </PostContext.Provider>
       </Switch>
+
       <Link to="/">Home</Link>
     </div>
   );
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,10 +1,32 @@
 import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
-const Posts = () => {
-  return (
-    <dib>
+const Posts = ({ querykey }) => {
+  const posts = useQuery(querykey, () => {
+    return axios
+      .get("http://localhost:8000/api/posts/")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  });
+
+  return posts.isLoading ? (
+    <div>
       <h1>Posts</h1>
-    </dib>
+      <h2>Loading...</h2>
+    </div>
+  ) : posts.isError ? (
+    <div>
+      <h1>Posts</h1>
+      <h2>{posts.error.message}</h2>
+    </div>
+  ) : (
+    <div>
+      <h1>Posts</h1>
+      {/* {posts.data.map((post) => {
+        console.log(post);
+      })} */}
+    </div>
   );
 };
 
